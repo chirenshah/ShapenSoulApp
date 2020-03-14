40adb8d5625caca98d3601ea4f0d77dee16fd9a0
Chiren Shah <55741062+chirenshah@users.noreply.github.com>
2020-03-15 00:58:50 +0530

    basic UI

--- App.js
+++ App.js
@@ -1,47 +1,147 @@
 import React, { Component } from 'react'
-import { View, Text, Button, TextInput } from 'react-native'
+import { View, Text, TouchableOpacity, TextInput , StyleSheet , Picker } from 'react-native'
 import PDF from './components/PDF'
-import FB from './components/Fb'
+import FB, { getUsers } from './components/Fb'
 import firebase from 'react-native-firebase'
 
-export default function App() {
+export default class App extends Component {
     state = {
         User:{
-            name:"Chiren"
+            name:"",
+            contact:"",
+            Address:"",
+            gender:"",
+            BloodG:""
         },
         userList:[]
     }
 
-    function addUser(user) {
-        console.log("User Added")
-        console.log(user)
+    getUserNotification = (userList) => {
+        console.log("User Added")        
+        this.setState(prevState => {
+            name: prevState.name = userList
+        })
+        
     }
 
-    async function readUsers() {
+    createUser = () => {
+        const User = {
+            name:this.state.name,
+            contact:this.state.contact,
+            Address:this.state.Address,
+            gender:this.state.gender,
+
+
+        }
+        console.log(User)
+    }
+
+    componentDidMount(){
+        getUsers(this.getUserNotification)
+        this.UNSAFE_componentWillMountisMounted = true
+    }
+
+    async readUsers() {
         var snapshot = await firebase.firestore()
                 .collection('Users')
                 .get()
         snapshot.forEach((data) => {
             const userItem = data.data()
-            this.state.userList.push(userItem)
+            //this.state.userList.push(userItem)
+            console.log(data);
         });
-        console.log(userList);
     }
+
+    render() {
         return(
-        <View>
-            <Text> Working?</Text>
-            <Button onPress={readUsers}
-                title="Find User"
+        <View style={styles.container}>
+            <View style={styles.header}>
+            <Text style={styles.text}> Profile</Text>
+            </View>
+            <TextInput placeholder="NAME" style={styles.input}
+                value={ this.state.name }
+                onChangeText={(text) =>{
+                this.setState({name:text})
+                }}
             />
-                <TextInput placeholder="NAME"
-                    value={ this.state.User.name }
-                onChangeText={(text) => 
-                    this.setState((prevState) => ({
-                        name : prevState.User.name = text
-                    }))
-                }
-                />
-            <Text>{this.state.User.name}</Text>
+            <TextInput placeholder="Contact No." style={styles.input}
+                value={ this.state.contact }
+                onChangeText={(text) =>{
+                    this.setState({contact:text})
+                }}
+            />
+            <TextInput placeholder="Address" style={styles.input}
+                value={ this.state.Address}
+                onChangeText={(text) =>{
+                    this.setState({Address:text})
+                }}
+            />
+            <View style={styles.input}>
+                <Picker
+                    selectedValue={this.state.gender}
+                    style={styles.picker}
+                    onValueChange={(itemValue, itemIndex) =>
+                        this.setState({gender: itemValue})
+                    }>
+                    <Picker.Item label="Male" value="Male" />
+                    <Picker.Item label="Female" value="Female" />
+                </Picker>
+            </View>
+            <TextInput placeholder="Blood Group" style={styles.input}
+                value={ this.state.BloodG }
+                onChangeText={(text) =>{
+                    this.setState({BloodG:text})
+                }}
+            />
+            <Text>{this.state.name}</Text>
+            <View style={styles.foo}>
+            <TouchableOpacity onPress={this.createUser} >
+                <Text style={{color:"white"}}>Submit</Text>
+            </TouchableOpacity>
+            </View>
             </View>
             )
+        }
     }
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        flexDirection:"column"
+    },
+    header:{
+        height:60,
+        padding:15,
+        backgroundColor: 'black'
+    },
+    text:{
+        color:'white',
+        fontSize:25,
+        textAlign:'center'
+    },
+    input:{
+        borderColor:"black",
+        borderWidth:1,
+        margin:20,
+        backgroundColor:'#e8eeef',
+        width:300,
+        alignSelf:'center',
+        textAlign:'center'
+    },
+    picker:{
+        paddingLeft:100,
+        width: 300,
+        color:'#C7C7CD',
+        textAlign:'center'
+    },
+    foo:{
+        width:200,
+        backgroundColor:"black",
+        padding:10,
+        paddingLeft:75,
+        textAlign:'center',
+        borderColor:"black",
+        borderWidth:1,
+        marginLeft:100
+    }
+})
